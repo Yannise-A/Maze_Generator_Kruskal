00744a1dc58dfc1ac35999bf89cc31e81e417d85
YanniseArbane <assaut7693@gmail.com>
2021-12-08 17:50:48 +0100

    Add boucle remplacement(fonctionne)

--- Maze_Generator.py
+++ Maze_Generator.py
@@ -38,53 +38,60 @@ def init_pixel(): #placer les pixels
     #print(pixel)
 
 def create_maze(): #Kruskal's algorithm
-    pixel[0][1] = 0 
+    pixel[0][1] = 0
+    pixel[1][1] = 0
     while is_finished() == True:
         x = random.randint(0, len(pixel)-2)
         y = random.randint(0, len(pixel)-2)
         if pixel[x][y] == -1:
             check(x,y)
+            print("================================\n",pixel)
             draw()
     pixel[maze_size-1][maze_size-2] = nb
 
 
 def check(x,y):
-    draw()
     div = x % 2
     if div == 0:
         if pixel[x-1][y] >= 0 and pixel[x+1][y]>= 0 and pixel[x-1][y] != pixel[x+1][y]:
             if pixel[x+1][y] > pixel[x-1][y]:
+                cell1 = pixel[x+1][y]
+                cell2 = pixel[x-1][y]
                 pixel[x][y] = pixel[x-1][y]
                 pixel[x+1][y] = pixel[x-1][y]
-                replace(x+1,y,pixel[x-1][y])
+                for i in range(maze_size):
+                    for j in range(maze_size):
+                        if pixel[i][j] == cell2 : 
+                            pixel[i][j] = cell1
             else:
+                cell1 = pixel[x-1][y]
+                cell2 = pixel[x+1][y]
                 pixel[x][y] = pixel[x+1][y]
                 pixel[x-1][y] = pixel[x+1][y]
-                replace(x-1,y,pixel[x+1][y])
+                for i in range(maze_size):
+                    for j in range(maze_size):
+                        if pixel[i][j] == cell2 : 
+                            pixel[i][j] = cell1
     else:
         if pixel[x][y-1] >= 0 and pixel[x][y+1] >= 0 and pixel[x][y-1] != pixel[x][y+1]:
             if pixel[x][y-1] > pixel[x][y+1]:
+                cell1 = pixel[x][y+1]
+                cell2 = pixel[x][y-1]
                 pixel[x][y] = pixel[x][y+1]
                 pixel[x][y-1] = pixel[x][y+1]
-                replace(x,y-1,pixel[x][y+1])
+                for i in range(maze_size):
+                    for j in range(maze_size):
+                        if pixel[i][j] == cell2 : 
+                            pixel[i][j] = cell1
             else:
+                cell1 = pixel[x][y-1]
+                cell2 = pixel[x][y+1]
                 pixel[x][y] = pixel[x][y-1]
                 pixel[x][y+1] = pixel[x][y-1]
-                replace(x,y+1,pixel[x][y-1])
-
-def replace(x,y,ref):
-    if pixel[x+1,y] != ref and pixel[x+1,y] != -1:
-        pixel[x+1,y] = ref
-        replace(x+1,y,ref)
-    if pixel[x-1,y] != ref and pixel[x-1,y] != -1:
-        pixel[x-1,y] = ref
-        replace(x-1,y,ref)
-    if pixel[x,y-1] != ref and pixel[x,y-1] != -1:
-        pixel[x,y-1] = ref
-        replace(x,y-1,ref)
-    if pixel[x,y+1] != ref and pixel[x,y+1] != -1:
-        pixel[x,y+1] = ref
-        replace(x,y-1,ref)
+                for i in range(maze_size):
+                    for j in range(maze_size):
+                        if pixel[i][j] == cell2 : 
+                            pixel[i][j] = cell1
 
 
 
